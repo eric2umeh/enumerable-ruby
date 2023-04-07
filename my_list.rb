f967d21a73501fb2f8afa5510ff6fb49ad973c24
hana <hanasabih@gmail.com>
2023-04-07 14:20:05 +0300

    fix errors

--- my_list.rb
+++ my_list.rb
@@ -1,9 +1,10 @@
-require_relative ‘my_enumerable’
+require_relative 'my_enumerable'
 class MyList
   include MyEnumerable
   def initialize(*list)
     @list = list
   end
+
   def each(&block)
     @list.each(&block)
   end
